1697f413508d9c860cd0ef8fefae35e9558247de
rayoungpark <rayoung1229@gmail.com>
2022-09-12 03:40:32 +0900

    💄 연, 월, 일, 요일 추가

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,19 @@
-const clock = document.querySelector("#clock");
+const today = document.querySelector('#today');
+const clock = document.querySelector('#clock');
 
+const getDate = () => {
+  const date = new Date();
+  today.innerText = `${date.toLocaleDateString(navigator.language, { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short' })}`;
+};
 const getClock = () => {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText=`${hours}:${minutes}:${seconds}`;
-}
+  const date = new Date();
+  if (new Date(today.innerText).getDate() !== date.getDate()) getDate();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+};
 
+getDate();
 getClock();
-setInterval(getClock,1000);
+setInterval(getClock, 1000);

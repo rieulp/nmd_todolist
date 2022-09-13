0b9503b63b479b458a286e7d05b9dd2d3146b54b
rayoungpark <rayoung1229@gmail.com>
2022-09-13 16:17:59 +0900

    🐛 Mixed content issue 해결

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,24 @@
-const API_KEY = "46edb1751a3390f4ba358f51ff5e7434";
+const API_KEY = '46edb1751a3390f4ba358f51ff5e7434';
 
 const onGeoOk = (position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-
-        console.log(data);
-        const ico = document.querySelector(".weather_ico");
-        const weather = document.querySelector("#weather span:nth-child(2)");
-        const city = document.querySelector("#weather span:last-child");
-        ico.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        ico.alt = data.weather[0].main;
-        city.innerText = data.name;
-        weather.innerText = `${data.main.temp}`;
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const ico = document.querySelector('.weather_ico');
+      const weather = document.querySelector('#weather span:nth-child(2)');
+      const city = document.querySelector('#weather span:last-child');
+      ico.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      ico.alt = data.weather[0].main;
+      city.innerText = data.name;
+      weather.innerText = `${data.main.temp}`;
     })
-    .catch(error => console.log("error", error))
-}
+    .catch((error) => console.log('error', error));
+};
 
-const onGeoFail = (error) => alert("Can't find you. No weather for you.");
+const onGeoFail = () => alert("Can't find you. No weather for you.");
 
 const currentPosition = navigator.geolocation.getCurrentPosition(onGeoOk, onGeoFail); //브라우저에서 위치 좌표를 줌
